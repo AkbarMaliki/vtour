9ef767e2224b13ca2181a075e570a4c01a61990e
AkbarMaliki <taufikakbarmalikitkj@gmail.com>
2021-12-14 15:09:37 +0800

    go

---
+++ docs/test1/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20211126_170537_00_merged_1",
+      "name": "IMG_20211126_170537_00_merged_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.7558034499940653,
+        "pitch": 0.05888949502432084,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5688467281469869,
+          "pitch": 0.06578141599467813,
+          "rotation": 0,
+          "target": "1-img_20211124_103049_00_merged_edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20211124_103049_00_merged_edited",
+      "name": "IMG_20211124_103049_00_merged_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.402759598694171,
+        "pitch": -0.16179740047085822,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7350099409830779,
+          "pitch": -0.12757635941763823,
+          "rotation": 0,
+          "target": "0-img_20211126_170537_00_merged_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
